d192486d2517f7ad23131bf1105ab26b0464dd67
Nick-palmar <66019522+Nick-palmar@users.noreply.github.com>
2020-11-17 22:19:08 -0500

    add insert methods for library

--- a5_library.hpp
+++ a5_library.hpp
@@ -3,15 +3,22 @@
 #include <vector>
 #include "a5_book.hpp"
 using namespace std;
- 
+
+// PURPOSE: Models a library which stores books
 class Library {
+    // atttributes
     vector<Book> book_list;
- 
+
 public:
+    // constructors
     Library();
     Library(vector<Book> new_book_list);
-    
+
+    // service methods
     void print();
- 
+    bool insert_book(Book new_book);
+    bool insert_book(string new_title, string new_authors, string new_dop);
+
 };
 #endif
+
