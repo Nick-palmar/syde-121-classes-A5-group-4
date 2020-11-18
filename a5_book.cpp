a0b411bfb4d6400bdc43b0ea6fe34533dad709ad
Nick-palmar <66019522+Nick-palmar@users.noreply.github.com>
2020-11-17 22:16:24 -0500

    Update a5_book.cpp
    add getters (impl) for books

--- a5_book.cpp
+++ a5_book.cpp
@@ -1,37 +1,39 @@
-#ifndef BOOKS
-#define BOOKS
-#include <string>
+#include "a5_book.hpp"
+#include <iostream>
 using namespace std;
 
-// PURPOSE: Models a book for reading
-class Book {
-	// attributes
-	string title, authors, dop;
+// book constructor definitions
+Book::Book() :
+    title("n/a"), authors("n/a"), dop("n/a") {}
 
-	// public methods of book class
-public:
-	// constructors
-	Book();
-	Book(string new_title);
-	Book(string new_title, string new_authors);
-	Book(string new_title, string new_authors, string new_dop);
-	// PURPOSE: initialize book objects. Note: this documentation applies to the 4 constructors above
-	//	   INPUTS: new_title - string referring to the book name (if applicable)
-	//			   new_authors - string referring to the names of the author(s) of the book (if applicable)
-	//             new_dop - string referring to the date of publication (if applicable)
-	//     OUTPUTS: none; constructors
+Book::Book(string new_title) :
+    title(new_title), authors("n/a"), dop("n/a") {}
 
-	// getters
-	string get_book_title();
-	string get_book_authors();
-	string get_book_dop();
+Book::Book(string new_title, string new_authors) :
+    title(new_title), authors(new_authors), dop("n/a") {}
 
-	// service methods
-	void print();
-	// PURPOSE: output the title, author names, and date of publication to the console
-	//     INPUTS: none
-	//	   OUTPUTS: none; output to console
+Book::Book(string new_title, string new_authors, string new_dop) :
+    title(new_title), authors(new_authors), dop(new_dop) {}
 
-};
-#endif
+// book getters definition 
+string Book::get_book_title() {
+    return title;
+}
 
+string Book::get_book_authors() {
+    return authors;
+}
+
+string Book::get_book_dop() {
+    return dop;
+}
+
+
+// book service method definition
+void Book::print() {
+    // output the book info to console
+    cout << "The book title is: " << title << endl;
+    cout << "The book authors is/are: " << authors << endl;
+    cout << "The book date of publication is: " << dop << endl;
+    cout << endl;
+}
