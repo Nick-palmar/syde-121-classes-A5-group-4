af53181a98f4b9ac436f9c9ced6a3cfc1377195b
Kevin Wang <70813550+kevinwang19@users.noreply.github.com>
2020-11-18 17:57:00 -0500

    Update a5_library.cpp

--- a5_library.cpp
+++ a5_library.cpp
@@ -19,23 +19,22 @@ void Library::print() {
 
 bool Library::insert_book(Book new_book) {
     // declare and initialize variables
-    bool is_repeated = false;
     bool same_authors, same_title, same_dop;
 
     // loop through each book in the library
     for (Book current_book : book_list) {
         // check if repeated
-        same_authors = current_book.get_book_authors() == new_book.get_book_authors();
         same_title = current_book.get_book_title() == new_book.get_book_title();
+        same_authors = current_book.get_book_authors() == new_book.get_book_authors();
         same_dop = current_book.get_book_dop() == new_book.get_book_dop();
 
-        if (same_authors && same_title && same_dop) {
+        if (same_title && same_authors && same_dop) {
             // do nothing if the same book is in the library; return false
             return false;
-        } 
+        }
     }
 
-    // the book is not in the library; insert it an return true
+    // the book is not in the library; insert it and return true
     book_list.push_back(new_book);
     return true;
 }
@@ -43,7 +42,6 @@ bool Library::insert_book(Book new_book) {
 
 bool Library::insert_book(string new_title, string new_authors, string new_dop) {
     // declare and initialize variables
-    bool is_repeated = false;
     bool same_authors, same_title, same_dop;
 
     // create a new book object
@@ -52,20 +50,71 @@ bool Library::insert_book(string new_title, string new_authors, string new_dop) 
     // loop through each book in the library
     for (Book current_book : book_list) {
         // check if repeated
-        same_authors = current_book.get_book_authors() == new_book.get_book_authors();
-        same_title = current_book.get_book_title() == new_book.get_book_title();
-        same_dop = current_book.get_book_dop() == new_book.get_book_dop();
+        same_title = current_book.get_book_title() == new_title;
+        same_authors = current_book.get_book_authors() == new_authors;
+        same_dop = current_book.get_book_dop() == new_dop;
 
-        if (same_authors && same_title && same_dop) {
+        if (same_title && same_authors && same_dop) {
             // do nothing if the same book is in the library; return false
             return false;
         }
     }
 
-    // the book is not in the library; insert it an return true
+    // the book is not in the library; insert it and return true
     book_list.push_back(new_book);
     return true;
 
 }
 
+bool Library::remove_book(Book searched_book) {
+    // declare and initialize variables
+    int index = 0;
+    bool same_authors, same_title, same_dop;
+
+    // loop through each book in the library
+    for (Book current_book : book_list) {
+        // check if repeated
+        same_title = current_book.get_book_title() == searched_book.get_book_title();
+        same_authors = current_book.get_book_authors() == searched_book.get_book_authors();
+        same_dop = current_book.get_book_dop() == searched_book.get_book_dop();
+
+        if (same_title && same_authors && same_dop) {
+            // the searched book is the same as a book in the library; remove the book from the list and return true
+            book_list.erase(book_list.begin() + index);
+            return true;
+        }
+        index ++;
+    }
+
+    // the searched book for removal is not in the library; return false
+    return false;
+}
+
+
+bool Library::remove_book(string searched_title, string searched_authors, string searched_dop) {
+    // declare and initialize variables
+    int index = 0;
+    bool same_authors, same_title, same_dop;
+
+    // create a new book object
+    Book searched_book(searched_title, searched_authors, searched_dop);
+
+    // loop through each book in the library
+    for (Book current_book : book_list) {
+        // check if repeated
+        same_title = current_book.get_book_title() == searched_title;
+        same_authors = current_book.get_book_authors() == searched_authors;
+        same_dop = current_book.get_book_dop() == searched_dop;
+
+        if (same_title && same_authors && same_dop) {
+            // the searched book is the same as a book in the library; remove the book from the list and return true
+            book_list.erase(book_list.begin() + index);
+            return true;
+        }
+        index ++;
+    }
+
+    // the searched book for removal is not in the library; return false
+    return false;
+}
 
