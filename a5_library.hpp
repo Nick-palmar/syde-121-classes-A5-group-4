2cd82eb3567698e1b010d31530c2de333e6b2927
Nick-palmar <66019522+Nick-palmar@users.noreply.github.com>
2020-11-20 11:28:40 -0500

    Update a5_library.hpp

--- a5_library.hpp
+++ a5_library.hpp
@@ -1,5 +1,21 @@
-#ifndef LIBRARY
-#define LIBRARY
+//***********************************************************
+// Student Names: Nicolas Palmar and Kevin Wang
+// Student Number: 20883350 and 20886511
+//
+// SYDE 121 Lab: Classes, OO Design Assignment: 5
+// Filename: a5_library.hpp
+// We hereby declare that this code, submitted for credit for the course
+// SYDE121, is a product of my own efforts.  This coded solution has
+// not been plagiarized from other sources and has not been knowingly
+// plagiarized by others
+//
+// Project: Book and Library Classes
+// Purpose: Declare methods for the library class which will store books
+// Due Date: Friday, November 20, 2020
+//**************************************************************
+
+#ifndef A5_LIBRARY_HPP
+#define A5_LIBRARY_HPP
 #include <vector>
 #include "a5_book.hpp"
 using namespace std;
@@ -22,24 +38,24 @@ public:
     // PURPOSE: output the books objects in the library
     //     INPUTS: none
     //     OUTPUTS: none; output to console
-    
+
     bool insert_book(Book new_book);
     // PURPOSE: checks if the book object already exists in the library
     //     INPUTS: new_book - book object that is being attemped to insert
     //     OUTPUTS: a boolean value; true if the book is not in the library and is able to be inserted otherwise false
-    
+
     bool insert_book(string new_title, string new_authors, string new_dop);
     // PURPOSE: checks if the book of string values already exists in the library
     //     INPUTS: new_title - string referring to the title of the book object that is being attemped to insert
     //             new_authors - string referring to the authors of the book object that is being attemped to insert
     //             new_dop - string referring to the date of publication of the book object that is being attemped to insert
     //     OUTPUTS: a boolean value; true if the book is not in the library and is able to be inserted otherwise false
-    
+
     bool remove_book(Book new_book);
     // PURPOSE: checks if the book object exists in the library
     //     INPUTS: new_book - book object that is being attemped to remove
     //     OUTPUTS: a boolean value; true if the book is in the library and is able to be removed otherwise false
-    
+
     bool remove_book(string new_title, string new_authors, string new_dop);
     // PURPOSE: checks if the book of string values exists in the library
     //     INPUTS: new_title - string referring to the title of the book object that is being attemped to remove
